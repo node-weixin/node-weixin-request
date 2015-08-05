03cae585906beab835710972b3a86e55ec08ea00
calidion <calidion@gmail.com>
2015-08-06 02:30:15 +0800

    init version

---
+++ README.md
@@ -0,0 +1,32 @@
+#  [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+
+> Requests for weixin apis
+
+
+## Install
+
+```sh
+$ npm install --save node-weixin-request
+```
+
+
+## Usage
+
+```js
+var nodeWeixinRequest = require('node-weixin-request');
+
+nodeWeixinRequest('Rainbow');
+```
+
+
+## License
+
+MIT © [JSSDKCN](blog.3gcnbeta.com)
+
+
+[npm-image]: https://badge.fury.io/js/node-weixin-request.svg
+[npm-url]: https://npmjs.org/package/node-weixin-request
+[travis-image]: https://travis-ci.org/JSSDKCN/node-weixin-request.svg?branch=master
+[travis-url]: https://travis-ci.org/JSSDKCN/node-weixin-request
+[daviddm-image]: https://david-dm.org/JSSDKCN/node-weixin-request.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/JSSDKCN/node-weixin-request
---
+++ index.js
@@ -0,0 +1,155 @@
+'use strict';
+var request = require('request');
+var fs = require("fs");
+var xml2json = require('xml2json');
+
+module.exports = {
+  /**
+   * Marshalling params
+   *
+   * @param params
+   * @returns {string}
+   */
+  toParam: function (params) {
+    params = params || {};
+    var keys = [];
+    for (var k in params) {
+      keys.push(encodeURIComponent(k) + '=' + encodeURIComponent(params[k]));
+    }
+    return keys.join('&');
+  },
+
+  /**
+   * For normal requests
+   * @param url
+   * @param data
+   * @param cb
+   */
+  request: function (url, data, cb) {
+    request.post({url: url, json: true, form: data}, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        //Return false if succeeded, else true
+        cb(false, body);
+      } else {
+        cb(true, {message: body});
+      }
+    });
+  },
+
+  /**
+   * For json data posting
+   *
+   * @param url
+   * @param data
+   * @param cb
+   */
+  json: function (url, data, cb) {
+    request.post({url: url, body: data, json: true}, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        cb(false, body);
+      } else {
+        cb(true, {message: body});
+      }
+    });
+  },
+
+  /**
+   * For xml data posting
+   *
+   * @param url
+   * @param data
+   * @param cb
+   */
+  xml: function (url, xml, cb) {
+    request.post({
+      url: url, body: xml, headers: {'Content-Type': 'text/xml'}
+    }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var json = null;
+        try {
+          json = JSON.parse(xml2json.toJson(body));
+          cb(false, json.xml);
+        } catch (e) {
+          console.log(e);
+          throw e;
+        }
+      } else {
+        cb(true, {message: body});
+      }
+    });
+  },
+
+  /**
+   * For xml data posting with ssl
+   *
+   * @param url
+   * @param data
+   * @param cb
+   */
+  xmlssl: function (url, xml, cb, ssl/*, passphrase*/) {
+    request.post({
+      url: url, body: xml, headers: {'Content-Type': 'text/xml'},
+      agentOptions: {
+        cert: ssl.cert,
+        key: ssl.key,
+        /*
+         pfx: ssl.pfx,
+         passphrase: ssl.passphrase || passphrase,
+         securityOptions: 'SSL_OP_NO_SSLv3'
+         */
+      }
+    }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var json = null;
+        try {
+          json = JSON.parse(xml2json.toJson(body));
+          cb(false, json.xml);
+        } catch (e) {
+          console.log(e);
+          throw e;
+        }
+      } else {
+        cb(true, {message: body});
+      }
+    });
+  },
+
+  /**
+   * For file uploading
+   *
+   * @param url
+   * @param file
+   * @param cb
+   */
+  file: function (url, file, cb) {
+    fs.stat(file, function (err) {
+      if (err) {
+        return cb(true, {message: 'File not exist'});
+      }
+      var media = fs.createReadStream(file);
+      request.post({
+        url: url,
+        json: true,
+        formData: {media: media, nonce: ''}
+      }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          cb(false, body);
+        } else {
+          cb(true, {message: body});
+        }
+      });
+    });
+  },
+
+  /**
+   * For file downloading
+   *
+   * @param url
+   * @param data
+   * @param file
+   * @param cb
+   */
+  download: function (url, data, file, cb) {
+    request.get({url: url, form: data}).pipe(fs.createWriteStream(file).on('finish', cb));
+  }
+};
